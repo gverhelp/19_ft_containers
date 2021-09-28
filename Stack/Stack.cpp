1fbd88e2d15e4f745431441ff4f0d1b8f45dac09
Garreth Verhelpen <gverhelp@student.s19.be>
2021-09-28 15:45:17 +0200

    Stack

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,2 +1,1 @@
 #include "Stack.hpp"
-
--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -2,14 +2,101 @@
 # define STACK_HPP
 
 # include <iostream>
+# include <deque>
 
 template<class T, class Container = std::deque<T>>
 class Stack
 {
     public:
+        ///// Member types /////
+        typedef T value_type;
+        typedef T& reference;
+        typedef const T& const_reference;
+        typedef Container container_type;
+        typedef size_t size_type;
+
+        ///// Member functions /////
+        explicit Stack(const container_type& ctnr = container_type()): C(ctnr) {}
+        virtual ~Stack();
+        Stack& operator=(const Stack& copy)
+        {
+            if (this != &copy)
+            {
+                this->C = copy.C;
+            }
+            return (*this);
+        }
+    
+        reference top()
+        {
+            return (this->C.back());
+        }
+
+        const_reference top()
+        {
+            return (this->C.back());
+        }
+
+        bool empty() const
+        {
+            return (this->C.empty());
+        }
+
+        size_type size() const
+        {
+            return (this->C.size());
+        }
+
+        void push(const value_type& val)
+        {
+            this->C.push_back(val);
+        }
+
+        void pop()
+        {
+            this->C.pop_back();
+        }
+
+        ///// Non member function : relational operators /////
+        template< class T, class Container >
+        bool operator==(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C == rhs.C);
+        }
+
+        template< class T, class Container >
+        bool operator!=(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C != rhs.C);
+        }
+	
+        template< class T, class Container >
+        bool operator<(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C < rhs.C);
+        }
+	
+        template< class T, class Container >
+        bool operator<=(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C <= rhs.C);
+        }
+
+        template< class T, class Container >
+        bool operator>(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C > rhs.C);
+        }
+
+        template< class T, class Container >
+        bool operator>=(const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs)
+        {
+            return (lhs.C >= rhs.C);
+        }
 
     private:
-        Container C;
+        ///// Member object /////
+        container_type C;
 };
 
 #endif
