cf2c60b0ace1e8099f61e6d4d671d45f0281908e
Garreth Verhelpen <gverhelp@student.s19.be>
2021-09-30 18:28:31 +0200

    Stack : everything looks good

--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -1,102 +1,87 @@
 #ifndef STACK_HPP
 # define STACK_HPP
 
-# include <iostream>
 # include <deque>
+# include <iostream>
 
-template<class T, class Container = std::deque<T> >
-class Stack
+namespace ft
 {
-    public:
-        ///// Member types /////
-        typedef T value_type;
-        typedef T& reference;
-        typedef const T& const_reference;
-        typedef Container container_type;
-        typedef size_t size_type;
+    template < class T, class Container = std::deque<T> >
+    class stack
+    {
+        public:
+            ///// Member types /////
+            typedef T value_type;
+            typedef Container container_type;
+            typedef size_t size_type;
 
-        ///// Member functions /////
-        explicit Stack(const container_type& ctnr = container_type()): C(ctnr) {}
-        virtual ~Stack();
-        Stack& operator=(const Stack& copy)
-        {
-            if (this != &copy)
+            ///// Member functions /////
+            explicit stack(const container_type& ctnr = container_type()): C(ctnr) {}
+            virtual ~stack() {}
+            stack& operator=(const stack& copy)
             {
-                this->C = copy.C;
+                if (this != &copy)
+                {
+                    this->C = copy.C;
+                }
+                return (*this);
             }
-            return (*this);
-        }
-    
-        reference top()
-        {
-            return (this->C.back());
-        }
+            value_type& top()
+            {
+                return (this->C.back());
+            }
+            const value_type& top() const
+            {
+                return (this->C.back());
+            }
+            bool empty() const
+            {
+                return (this->C.empty());
+            }
+            size_type size() const
+            {
+                return (this->C.size());
+            }
+            void push(const value_type& val)
+            {
+                this->C.push_back(val);
+            }
+            void pop()
+            {
+                this->C.pop_back();
+            }
 
-        const_reference top() const
-        {
-            return (this->C.back());
-        }
+        private:
+            ///// Non member function : relational operators /////
+            friend bool operator==(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C == rhs.C);
+            }
+            friend bool operator!=(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C != rhs.C);
+            }
+            friend bool operator<(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C < rhs.C);
+            }
+            friend bool operator<=(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C <= rhs.C);
+            }
+            friend bool operator>(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C > rhs.C);
+            }
+            friend bool operator>=(const stack& lhs, const stack& rhs)
+            {
+                return (lhs.C >= rhs.C);
+            }
 
-        bool empty() const
-        {
-            return (this->C.empty());
-        }
-
-        size_type size() const
-        {
-            return (this->C.size());
-        }
-
-        void push(const value_type& val)
-        {
-            this->C.push_back(val);
-        }
-
-        void pop()
-        {
-            this->C.pop_back();
-        }
-
-        ///// Non member function : relational operators /////
-        template< class stack_type, class container_type >
-        bool operator==(const Stack<stack_type, container_type>& rhs)
-        {
-            return (this->C == rhs.C);
-        }
-
-        template< class stack_type, class container_type >
-        bool operator!=(const Stack<stack_type, container_type>& rhs)
-        {
-            return (this->C != rhs.C);
-        }
-	
-        template< class stack_type, class container_type >
-        bool operator<(const Stack<stack_type, container_type>& rhs)
-        {
-            return (this->C < rhs.C);
-        }
-	
-        template< class stack_type, class container_type >
-        bool operator<=(const Stack<stack_type, container_type>& rhs)
-        {
-            return (this->C <= rhs.C);
-        }
-
-        template< class stack_type, class container_type >
-        bool operator>(const Stack<stack_type, container_type>& rhs)
-        {
-            return (this->C > rhs.C);
-        }
-
-        template< class stack_type, class container_type >
-        bool operator>=(const Stack<stack_type, Container>& rhs)
-        {
-            return (this->C >= rhs.C);
-        }
-
-    private:
-        ///// Member object /////
-        container_type C;
-};
+        protected:
+                ///// Member object /////
+                container_type C;
+    };
+}
 
 #endif
--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,13 +1,35 @@
-#include "Stack.hpp"
+#include "./stack.hpp"
+#include <vector>
+#include <stack>
 
 int main()
 {
-    Stack<int> stack;
+    ft::stack<int> stack;
 
     std::cout << "Stack size : " << stack.size() << std::endl;
     stack.push(10);
     stack.push(20);
+    stack.push(30);
+    stack.push(40);
     std::cout << "Stack size : " << stack.size() << std::endl;
+    stack.pop();
+    std::cout << "Stack size : " << stack.size() << std::endl;
+
+    ft::stack<int> stack2(stack);
+
+    while (!stack.empty())
+    {
+        std::cout << ' ' << stack.top();
+        stack.pop();
+    }
+    std::cout << "\nStack size : " << stack.size() << std::endl;
+
+    while (!stack2.empty())
+    {
+        std::cout << ' ' << stack2.top();
+        stack2.pop();
+    }
+    std::cout << "\nStack size : " << stack2.size() << std::endl;
 
     return (0);
 }
