d111d0335c87af59982d859af7ec577be1efcdcd
gverhelp <garreth.verhelpen@gmail.com>
2021-12-07 02:44:59 +0100

    DONE??

--- include/utils/BTree.hpp
+++ include/utils/BTree.hpp
@@ -15,7 +15,7 @@ namespace ft
             typedef Key                                     key_type;
             typedef ft::pair<const key_type, mapped_type>   value_type;
             typedef std::allocator< Node<value_type> >      node_allocator;
-            typedef ft::Node<value_type>*					node_pointer;
+            typedef ft::Node<value_type>*                   node_pointer;
             typedef ft::Node<value_type>                    node_type;
 
             //------------------- Constructor and destructor -------------------//
